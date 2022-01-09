2b4be2eeff45b622b5443e70d23693bd7e210670
Robin <philoin88@gmail.com>
2022-01-10 00:04:18 +0900

    Set for textarea and function for coding and executing

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,68 @@
-import React from 'react';
+import * as esbuild from 'esbuild-wasm';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
+import { fetchPlugin } from './plugins/fetch-plugin';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  const ref = useRef<any>();
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  const startService = async () => {
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
+    });
+  };
+
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    // const result = await ref.current.transform(input, {
+    //   loader: 'jsx',
+    //   target: 'es2015',
+    // });
+
+    const result = await ref.current.build({
+      entryPoints: ['index.js'],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
+      define: {
+        'process.env.NODE_ENV': '"production"',
+        global: 'window',
+      },
+    });
+
+    console.log('result', result);
+    setCode(result.outputFiles[0].text);
+  };
+
+  return (
+    <div>
+      <textarea
+        name="textarea"
+        id="textarea"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+
+      <div>
+        <button type="submit" onClick={onClick}>
+          Submit
+        </button>
+      </div>
+
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,33 @@
+import * as esbuild from 'esbuild-wasm';
+
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      // Handle root entry file of 'index.js'
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
+
+      // Handle relative paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        // if it is looking for a relative path of a library that is depended by other libraries
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href,
+          };
+        }
+      });
+
+      // Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+    },
+  };
+};
